10b15b692cbc18e7cd260a2385d430504fe73ea0
Emma-xiaochen <736202625@qq.com>
2021-12-03 20:02:44 +0800

    添加路由

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ const { APP_PORT } = require('./config/config.default');
 
 const app = new Koa();
 
-app.use((ctx, next) => {
-  ctx.body = 'hello api!';
-})
+const userRouter = require('./router/user.route');
+
+app.use(userRouter.routes())
 
 app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`);
