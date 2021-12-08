b7cf8e036b2c8c0c191f0e670112708e5773479d
Emma-xiaochen <736202625@qq.com>
2021-12-08 16:39:59 +0800

    添加用户

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,10 +3,22 @@
  * @author: Emma
  */
 
+const User = require('../model/user.model')
+
 class UserService {
   async createUser(user_name, password) {
-    // todo: 写入数据库
-    return '写入数据库成功';
+    // 插入数据
+    // User.create({
+    //   // 表的字段
+    //   user_name: user_name,
+    //   password: password
+    // })
+
+    // async表达式： promise对象的值
+    const res = await User.create({ user_name, password });
+    // console.log(res);
+
+    return res.dataValues;
   }
 }
 
