f35eef977de16f4c5a4d62f9570385bf7ae51e17
Emma-xiaochen <736202625@qq.com>
2021-12-09 19:57:03 +0800

    拆分中间件

---
+++ src/app/errHandler.js
@@ -0,0 +1,20 @@
+/**
+ * @description: 错误处理内容
+ * @author: Emma
+ */
+
+module.exports = (err, ctx) => {
+  let status = 500;
+  switch (err.code) {
+    case '10001':
+      status = 400;
+      break;
+    case '10002':
+      status = 409;
+      break;
+    default:
+      status = 500;
+  }
+  ctx.status = status;
+  ctx.body = err;
+}
