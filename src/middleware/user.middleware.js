11323984962f87d994073f31a2b8b4c6d0b79378
Emma-xiaochen <736202625@qq.com>
2021-12-09 19:57:30 +0800

    拆分中间件

---
+++ src/middleware/user.middleware.js
@@ -0,0 +1,36 @@
+/**
+ * @description: 用户模块相关的中间件
+ * @author: Emma
+ */
+
+const { getUserInfo } = require('../service/user.service');
+const { userFormateError, userAlreadyExited } = require('../consitant/err.type');
+
+// 用户验证器
+const userValidator = async (ctx, next) => {
+  const { user_name, password } = ctx.request.body;
+
+  if (!user_name || !password) {
+    console.error('用户名或密码为空', ctx.request.body);
+    ctx.app.emit('error', userFormateError, ctx);
+    return;
+  }
+  
+  await next();
+}
+
+// 验证用户
+const verifyUser = async (ctx, next) => {
+  const { user_name } = ctx.request.body;
+
+  if (getUserInfo({ user_name })) {
+    ctx.app.emit('error', userAlreadyExited, ctx);
+    return;
+  }
+  await next();
+}
+
+module.exports = {
+  userValidator,
+  verifyUser
+}
