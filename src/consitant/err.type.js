36d83abae7504f180db8e0d46136dad3df5f0b77
Emma-xiaochen <736202625@qq.com>
2021-12-09 19:57:17 +0800

    拆分中间件

---
+++ src/consitant/err.type.js
@@ -0,0 +1,17 @@
+/**
+ * @description: 错误类型
+ * @author: Emma
+ */
+
+module.exports = {
+  userFormateError: {
+    code: '10001',
+    message: '用户名或密码为空',
+    result: ''
+  },
+  userAlreadyExited: {
+    code: '10002',
+    message: '用户已经存在',
+    result: ''
+  }
+}
