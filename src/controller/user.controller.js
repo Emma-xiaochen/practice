fe298c1e5022ff646d6ff642edbbc0cf9f821c76
Emma-xiaochen <736202625@qq.com>
2021-12-16 18:57:33 +0800

    错误处理的补充与完善

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,7 +3,9 @@
  * @author: Emma
  */
 
-const { createUser, getUserInfo } = require('../service/user.service')
+const { createUser } = require('../service/user.service')
+
+const { userRegisterError } = require('../constant/err.type')
 
 class UserController {
   async register(ctx, next) {
@@ -12,17 +14,22 @@ class UserController {
     const { user_name, password } = ctx.request.body;
 
     // 2. 操作数据库
-    const res = await createUser(user_name, password);
-    console.log(res);
-    // 3. 返回结果
-    ctx.body = {
-      code: 0,
-      message: '用户注册成功',
-      result: {
-        id: res.id,
-        user_name: res.user_name,
-      }
-    };
+    try {
+      const res = await createUser(user_name, password);
+      // console.log(res);
+      // 3. 返回结果
+      ctx.body = {
+        code: 0,
+        message: '用户注册成功',
+        result: {
+          id: res.id,
+          user_name: res.user_name,
+        }
+      };
+    } catch(err) {
+      console.log(err);
+      ctx.app.emit('error', userRegisterError, ctx);
+    }
   }
 
   async login(ctx, next) {
