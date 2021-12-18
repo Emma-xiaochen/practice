d870ccb557af4e10865d621c6d4f2a483f662393
Emma-xiaochen <736202625@qq.com>
2021-12-18 23:59:13 +0800

    密码加密

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -5,13 +5,17 @@
 
 const Router = require('koa-router');
 
-const { userValidator, verifyUser } = require('../middleware/user.middleware');
+const {
+  userValidator,
+  verifyUser,
+  cryptPassword
+} = require('../middleware/user.middleware');
 const { register, login } = require('../controller/user.controller');
 
 const router = new Router({ prefix: '/users' });
 
 // 注册接口
-router.post('/register', userValidator, verifyUser, register);
+router.post('/register', userValidator, verifyUser, cryptPassword, register);
 
 // 登录接口
 router.post('/login', login);
