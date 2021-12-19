1b37e79c063344fa3b665603c289f4da747329b4
Emma-xiaochen <736202625@qq.com>
2021-12-19 19:08:10 +0800

    登录验证

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -5,7 +5,14 @@
 
 const bcrypt = require('bcryptjs');
 const { getUserInfo } = require('../service/user.service');
-const { userFormateError, userAlreadyExited, userRegisterError } = require('../constant/err.type');
+const {
+  userFormateError,
+  userAlreadyExited,
+  userRegisterError,
+  userDoesNotExist,
+  userLoginError,
+  invalidPassword
+} = require('../constant/err.type');
 
 // 用户验证器
 const userValidator = async (ctx, next) => {
@@ -57,8 +64,37 @@ const cryptPassword = async(ctx, next) => {
   await next();
 }
 
+// 验证登录
+const verifyLogin = async (ctx, next) => {
+  // 1. 判断用户是否存在(不存在：报错)
+  const { user_name, password } = ctx.request.body;
+
+  try {
+    const res = await getUserInfo({ user_name });
+
+    if (!res) {
+      console.error('用户名不存在', { user_name });
+      ctx.app.emit('error', userDoesNotExist, ctx);
+      return;
+    }
+
+    // 2. 密码是否匹配(不匹配：报错)
+    if (!bcrypt.compareSync(password, res.password)) {
+      ctx.app.emit('error', invalidPassword, ctx);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    ctx.app.emit('error', userLoginError, ctx);
+    return;
+  }
+
+  await next();
+}
+
 module.exports = {
   userValidator,
   verifyUser,
-  cryptPassword
+  cryptPassword,
+  verifyLogin
 }
