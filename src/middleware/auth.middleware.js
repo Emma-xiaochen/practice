0a201d9b5bfb94e22ddf8aa58d04938b325df556
Emma-xiaochen <736202625@qq.com>
2022-01-08 14:34:30 +0800

    封装管理员权限

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,7 +7,11 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = require('../config/config.default');
 
-const { tokenExpiredError, invalidToken } = require('../constant/err.type');
+const {
+  tokenExpiredError,
+  invalidToken,
+  hasNotAdminPermission
+} = require('../constant/err.type');
 
 const auth = async (ctx, next) => {
   const { authorization } = ctx.request.header;
@@ -32,6 +36,18 @@ const auth = async (ctx, next) => {
   await next();
 }
 
+const hadAdminPermission = async (ctx, next) => {
+  const {is_admin} = ctx.state.user;
+
+  if (!is_admin) {
+    console.error('该用户没有管理员权限', ctx.state.user);
+    return ctx.app.emit('error', hasNotAdminPermission, ctx);
+  }
+
+  await next();
+}
+
 module.exports = {
-  auth
+  auth,
+  hadAdminPermission
 }
