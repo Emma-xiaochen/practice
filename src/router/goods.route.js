407e2fe852dcdeefac4ca8d92e2ae0f343f8fcb6
Emma-xiaochen <736202625@qq.com>
2022-01-17 15:49:19 +0800

    集成统一的参数格式校验

--- src/router/goods.route.js
+++ src/router/goods.route.js
@@ -1,13 +1,20 @@
 const Router = require('koa-router');
 
 const { auth, hadAdminPermission } = require('../middleware/auth.middleware');
+const { validator } = require('../middleware/goods.middleware');
+
 
 const { upload } = require('../controller/goods.controller');
 
 const router = new Router({ prefix: '/goods' });
 
-// router.post('/upload', auth, hadAdminPermission, upload);
-router.post('/upload', upload);
+// 商品图片上传接口
+router.post('/upload', auth, hadAdminPermission, upload);
+
+// 发布商品接口
+router.post('/', auth, hadAdminPermission, validator, (ctx) => {
+  ctx.body = '发布商品成功'
+});
 
 
 module.exports = router;
