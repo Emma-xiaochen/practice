8ed67045cb31c3642eac674523b85fb8a5c06650
Emma-xiaochen <736202625@qq.com>
2022-01-28 21:34:05 +0800

    修改商品接口

--- src/router/goods.route.js
+++ src/router/goods.route.js
@@ -4,7 +4,7 @@ const { auth, hadAdminPermission } = require('../middleware/auth.middleware');
 const { validator } = require('../middleware/goods.middleware');
 
 
-const { upload, create } = require('../controller/goods.controller');
+const { upload, create, update } = require('../controller/goods.controller');
 
 const router = new Router({ prefix: '/goods' });
 
@@ -14,5 +14,7 @@ router.post('/upload', auth, hadAdminPermission, upload);
 // 发布商品接口
 router.post('/', auth, hadAdminPermission, validator, create);
 
+// 修改商品接口
+router.put('/:id', auth, hadAdminPermission, validator, update);
 
 module.exports = router;
